d00f9f894f68d42c85781237db02a0c6105b57fe
Md Shayon <mdshayon0@gmail.com>
2019-11-02 02:08:33 +0600

    todo lis with crud

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+export class TodoItem extends Component {
+
+
+
+  getStyle = () => {
+    return {
+      background: 'gray',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    }
+
+
+    /*if (this.props.todo.completed) {
+      return {
+        textDecoration: 'line-through'
+      }
+    } else {
+      return {
+        textDecoration: 'none'
+      }
+    }*/
+
+  }
+
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      <div style={this.getStyle()} st>
+        <p>
+            <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />
+             {' '} 
+             {title}
+             <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>X</button>
+         </p>
+      </div>
+    )
+  }
+}
+
+
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+
+
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 8px',
+  borderRadius: '50%',
+  curser: 'pointer',
+  float: 'right'
+}
+
+// const itemStyle = {
+//   backgroundColor: 'yellow'
+// }
+
+export default TodoItem;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <header style= {{background:'#333', color: 'white', textAlign:'center', padding: '10px'}}>
+            <h1>Todo List</h1>
+        </header>
+    );
+}
+
+
+export default Header; 
