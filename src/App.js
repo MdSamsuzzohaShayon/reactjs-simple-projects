214394751fa2781485936c1fd2c9a53e0fd2a7c8
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-12-15 19:58:05 +0600

    incomplete project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Board from './components/card-board/Board';
+import Card from './components/card-board/Card';
+
 import CardSlide from './components/CardSlide';
 
 function App() {
   return (
     <div className="App">
+
+      {/* Incomplete */}
+      {/* <Board id="board-1" className="board" >
+        <Card id="card-1" className="card" draggable="true">
+          <p>Card One</p>
+        </Card>
+      </Board>
+      <Board id="board-1" className="board" >
+        <Card id="card-1" className="card" draggable="true">
+          <p>Card Two</p>
+        </Card>
+      </Board> */}
+
+
+
+
       <CardSlide />
     </div>
   );
---
+++ src/components/card-board/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Card(props) {
+    const dragStart = e =>{
+        const target = e.target;
+        e.dataTransfer.setData("card_id", target.id);
+        setTimeout(()=>{
+            target.style.display = "none";
+        }, 0)
+    }
+
+
+    const dragOver = e =>{
+        e.stopPropagation();
+    }
+    return (
+        <div
+            id={props.id}
+            className={props.className}
+            onDragStart={dragStart}
+            draggable={props.draggable}
+            onDragOver={dragOver}
+            
+        >
+            {props.childern}
+        </div>
+    )
+}
+
+export default Card;
